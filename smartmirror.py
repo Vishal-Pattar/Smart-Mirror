fc5131fc20338da8b31fce6f946107052b3b27cc
Vishal-Pattar <vmpnwc15002@gmail.com>
2025-03-21 13:57:23 +0530

    Fixed bug

--- smartmirror.py
+++ smartmirror.py
@@ -48,7 +48,10 @@ def setlocale(name):
     with LOCALE_LOCK:
         saved = locale.setlocale(locale.LC_ALL)
         try:
-            yield locale.setlocale(locale.LC_ALL, name)
+            if name in locale.locale_alias:
+                yield locale.setlocale(locale.LC_ALL, name)
+            else:
+                raise locale.Error(f"Locale '{name}' not supported.")
         finally:
             locale.setlocale(locale.LC_ALL, saved)
 
